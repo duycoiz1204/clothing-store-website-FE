0a71d488dad164149c8e1433f2eb94cc9bde4ba1
Nguyễn Trọng Đức <acedurex2002@gmail.com>
2023-02-17 11:31:25 +0700

    create header-footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,74 @@
 import classNames from 'classnames/bind';
-
+import { FontAwesomeIcon }  from '@fortawesome/react-fontawesome';
 import styles from './Footer.module.scss';
+import image from '~/assets/images/logo-large.png'
+import { faSquareFacebook, faSquareInstagram } from '@fortawesome/free-brands-svg-icons';
+import { Grid, Row, Column } from '~/components/Grid';
 
 const cx = classNames.bind(styles);
 
 function Footer() {
-    return <div className={cx('container')}>Footer</div>;
+    return <div className={cx('container')}>
+        <Grid className={cx('wide')}>
+            <Row>
+                <Column className={cx('l-4')}>
+                    <div className={cx("content-left")}>
+                        <div className={cx("content-link")}>
+                            <a href="#">Home</a>
+                            <a href="#">Product</a>
+                            <a href="#">Promotion</a>
+                        </div>
+                        <h2 className={cx("heading")}>Information</h2>
+                        <div className={cx("information")}>
+                            <p>
+                                19 Tran Xuan Soan, Ward 5, District 3, Ho Chi Minh City, Vietnam
+                            </p> 
+                            <p> 
+                                0919011022
+                            </p> 
+                            <p> 
+                                clothingstore@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </Column>
+                <Column className= {cx("l-4")}>
+                    <div className={cx("content-between")}>
+                        <div className={cx("logo")}>
+                            <img src={image} alt="" className={cx("img-logo")}></img>
+                        </div>
+                        <p className={cx("reserved")}>All rights reserved</p>
+                    </div>
+                </Column>
+                <Column className={cx("l-4")}>
+                    <div className={cx("content-right")}>
+                        <div>
+                            <a href="#">About</a>
+                            <a href="#">Contact</a>
+                            <a href="#">Items</a>
+                        </div>
+                        <h2 className={cx("heading")}>Social</h2>
+                        <div className={cx("facebook-link")}>
+                            <div>
+                            <FontAwesomeIcon icon={faSquareFacebook} className={cx("icon-face")}/> 
+                            </div>
+                            <div className={cx("icon-block")}>
+                                Facebook
+                            </div>
+                        </div>
+                        <div className={cx("instagram-link")}>
+                            <div>
+                            <FontAwesomeIcon icon={faSquareInstagram} className={cx("icon-instagram")}/> 
+                            </div>
+                            <div className={cx("icon-block")}>
+                                Instagram
+                            </div>
+                        </div>
+                    </div>
+                </Column>
+            </Row>
+        </Grid>
+    </div>
 }
 
 export default Footer;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,53 @@ import classNames from 'classnames/bind';
 
 import styles from './Header.module.scss';
 
+import image from '~/assets/images/logo.png'
+
+import { Grid, Row, Column } from '~/components/Grid'
+
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <div className={cx('container')}>Header</div>;
+    return <div className={cx('container')}>
+        <Grid className={cx('wide')}>
+             <Row>
+                <Column className={cx('l-5')}>
+                    <div className={cx("left-content")}>
+                        <div className={cx("logo")}>
+                            <img className={cx("img-logo")} src={image} alt=""></img>
+                        </div>
+                        <li className={cx("text-item")}>
+                            <a href="#">About</a>
+                        </li>
+                        <li className={cx("text-item")}>
+                            <a href="#">Product</a>
+                        </li>
+                        <li className={cx("text-item")}>
+                            <a href="#">Best sellers</a>
+                        </li>
+                        <li className={cx("text-item")}>
+                            <a href="#">Contact</a>
+                        </li>
+                    </div>
+                </Column>
+                <Column className={cx("l-3")}></Column>
+                <Column className={cx("l-4")}>
+                    <div className={cx("right-content")}>
+                        <div className="">
+                            <button className={cx("btn")}>Login</button>
+                        </div>
+                        <div className="">
+                            <button className={cx("btn")}>Sign Up</button>
+                        </div>
+                        <div className={cx("cart-icon")}>
+                            <span><i className={cx("fa-solid fa-cart-shopping  icon")}></i></span>
+                        </div>
+                    </div>
+
+                </Column>
+            </Row>
+        </Grid>
+    </div>;
 }
 
 export default Header;
